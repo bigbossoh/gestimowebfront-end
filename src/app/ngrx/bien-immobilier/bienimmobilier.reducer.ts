0b248d3f1d865f800cab506459bc21c6181c0f52
zrangonazaire <astairenazaire@gmail.com>
2022-05-12 17:34:51 +0000

    Push ngrx

---
+++ src/app/ngrx/bien-immobilier/bienimmobilier.reducer.ts
@@ -0,0 +1,44 @@
+import { Bienimmobilier } from 'src/gs-api/src/models';
+import {
+  BienImmobilierActions,
+  BienImmobilierActionsTypes,
+} from './bienimmobilier.actions';
+export enum BienImmobilierStateEnum {
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+}
+export interface BienImmobilierState {
+  bienImmoblilier: Bienimmobilier[];
+  errorMessage: string;
+  dataState: BienImmobilierStateEnum;
+}
+const initState: BienImmobilierState = {
+  bienImmoblilier: [],
+  errorMessage: '',
+  dataState: BienImmobilierStateEnum.INITIAL,
+};
+export function bienReducer(
+  state: BienImmobilierState = initState,
+  action: BienImmobilierActions
+) {
+  switch (action.type) {
+    case BienImmobilierActionsTypes.GET_ALL_BIENS:
+      return { ...state, dataState: BienImmobilierStateEnum.LOADED };
+    case BienImmobilierActionsTypes.GET_ALL_BIENS_SUCCES:
+      return {
+        ...state,
+        dataState: BienImmobilierStateEnum.LOADED,
+        bienImmoblilier: action.payload,
+      };
+    case BienImmobilierActionsTypes.GET_ALL_BIENS_ERROR:
+      return {
+        ...state,
+        dataState: BienImmobilierStateEnum.ERROR,
+        errorMessage: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+}
