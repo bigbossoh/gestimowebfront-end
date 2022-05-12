1f195af880c39b69ffb657a90071a99dc66d30a8
zrangonazaire <astairenazaire@gmail.com>
2022-05-12 18:24:05 +0000

    ngrx 2

--- src/app/ngrx/bien-immobilier/bienimmobilier.effects.ts
+++ src/app/ngrx/bien-immobilier/bienimmobilier.effects.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 import { ApiService } from 'src/gs-api/src/services';
-import { BienImmobilierActionsTypes } from './bienimmobilier.actions';
+import {
+  BienImmobilierActionsTypes,
+  GetAllBiensActionsError,
+  GetAllBiensActionsSuccess,
+} from './bienimmobilier.actions';
 
-// @Injectable()
-// export class BienVillaEffects {
-//   constructor(private apiService: ApiService, private effectActions: Actions) {}
-//   getAllBienseffect:Observable<Action>=createEffect(()=> this.effectActions.pipe(
-// ofType(BienImmobilierActionsTypes.GET_ALL_BIENS),
-// mergeMap((action)=>{
-//   return this.apiService.sav
-// })
-//   ));
-// }
+@Injectable()
+export class BienVillaEffects {
+  constructor(private apiService: ApiService, private effectActions: Actions) {}
+  getAllBienseffect: Observable<Action> = createEffect(() =>
+    this.effectActions.pipe(
+      ofType(BienImmobilierActionsTypes.GET_ALL_BIENS),
+      mergeMap((action) => {
+        return this.apiService.findAllBien().pipe(
+          map((biens) => new GetAllBiensActionsSuccess(biens)),
+          catchError((err) => of(new GetAllBiensActionsError(err)))
+        );
+      })
+    )
+  );
+}
