ddb352b3fa58aeb17d49b6f497b8a9f3937fff87
bigbossoh <michel.bossoh@outlook.fr>
2022-05-13 17:22:30 +0000

    done with adding new user

--- src/app/pages/utilisateurs/page-liste-utilisateurs/page-liste-utilisateurs.component.ts
+++ src/app/pages/utilisateurs/page-liste-utilisateurs/page-liste-utilisateurs.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { NotificationType } from 'src/app/enum/natification-type.enum';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { UserService } from 'src/app/services/user/user.service';
+import { UtilisateurRequestDto } from 'src/gs-api/src/models';
 import { ApiService } from 'src/gs-api/src/services';
 import { Utilisateur } from '../../../../gs-api/src/models/utilisateur';
 
@@ -30,16 +31,17 @@ export class PageListeUtilisateursComponent implements OnInit , OnDestroy {
 
 
   ngOnInit(): void {
+    this.user = this.userService.getUserFromLocalCache();
     this.getUsers(true);
   }
 
-  public getUsers(showNotification: boolean): void {
+    public getUsers(showNotification: boolean): void {
       this.refreshing = true;
       this.subscriptions.push(
         this.userService.getUsers().subscribe(
           (response: any) => {
             console.log(response);
-            this.userService.addUserToLocalCache(response);
+           // this.userService.addUserToLocalCache(response);
             this.users = response;
             this.refreshing = false;
             if (showNotification) {
@@ -61,6 +63,34 @@ export class PageListeUtilisateursComponent implements OnInit , OnDestroy {
     private clickButton(buttonId: string): void {
       document.getElementById(buttonId)!.click();
     }
+    public saveNewUser(): void {
+      this.clickButton('new-user-save');
+    }
+    public onAddNewUser(userForm: UtilisateurRequestDto): void {
+      console.log("Im in inadduser fonction ",this.user,this.user?.id);
+
+     userForm.userCreate=this.user?.id;
+     userForm.agenceDto=this.user?.idAgence;
+    userForm.mobile=userForm.username;
+    userForm.id=0
+
+      console.log("adding mobil",userForm);
+      this.subscriptions.push(
+        this.userService.addUser(userForm).subscribe(
+          (response: any) => {
+            this.clickButton('new-user-close');
+            this.getUsers(true);
+
+            //userForm.reset();
+            this.sendErrorNotification(NotificationType.SUCCESS, `${response.firstName} ${response.lastName} added successfully`);
+          },
+          (errorResponse: HttpErrorResponse) => {
+            this.sendErrorNotification(NotificationType.ERROR, errorResponse.error.message);
+            this.profileImage = null;
+          }
+        )
+        );
+    }
     private sendErrorNotification(notificationType: NotificationType, message: string): void {
       if (message) {
         this.notificationService.notify(notificationType, message);
@@ -68,6 +98,7 @@ export class PageListeUtilisateursComponent implements OnInit , OnDestroy {
         this.notificationService.notify(notificationType, 'An error occurred. Please try again.');
       }
     }
+
   ngOnDestroy(): void {
     throw new Error('Method not implemented.');
   }
