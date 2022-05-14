48cc4c217af76d7ed0ea31c1211b75aa1407a5d8
bigbossoh <michel.bossoh@outlook.fr>
2022-05-14 16:49:23 +0000

    Still working on list des users

---
+++ src/app/pages/reglement/page-paiement/page-paiement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-paiement',
+  templateUrl: './page-paiement.component.html',
+  styleUrls: ['./page-paiement.component.css']
+})
+export class PagePaiementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
