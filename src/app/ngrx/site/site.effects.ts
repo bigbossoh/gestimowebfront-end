ae422227c3540ea59f9475a608025078600b448e
zrangonazaire <astairenazaire@gmail.com>
2022-05-14 16:29:47 +0000

    feature-create-new-bien v1 not finish

---
+++ src/app/ngrx/site/site.effects.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import { ApiService } from 'src/gs-api/src/services';
+import {
+  SiteActionsTypes,
+  GetAllSitesActionsError,
+  GetAllSitesActionsSuccess,
+} from './site.actions';
+
+@Injectable()
+export class SiteEffects {
+  constructor(private apiService: ApiService, private effectActions: Actions) {}
+  getAllBienseffect: Observable<Action> = createEffect(() =>
+    this.effectActions.pipe(
+      ofType(SiteActionsTypes.GET_ALL_SITES),
+      mergeMap((action) => {
+        return this.apiService.findAllSites().pipe(
+          map((sites) => new GetAllSitesActionsSuccess(sites)),
+          catchError((err) => of(new GetAllSitesActionsError(err)))
+        );
+      })
+    )
+  );
+}
---
+++ src/app/ngrx/utulisateur/utlisateur.reducer.ts
@@ -0,0 +1,49 @@
+import { Action } from '@ngrx/store';
+import {
+  BienImmobilierDto,
+  SiteResponseDto,
+  UtilisateurRequestDto,
+} from 'src/gs-api/src/models';
+import {
+  UtilisateurActionsTypes,
+  UtilisateurActions,
+} from './utilisateur.actions';
+export enum UtilisteurStateEnum {
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+}
+export interface UtilisteurState {
+  utlisisateurs: UtilisateurRequestDto[];
+  errorMessage: string;
+  dataState: UtilisteurStateEnum;
+}
+const initState: UtilisteurState = {
+  utlisisateurs: [],
+  errorMessage: '',
+  dataState: UtilisteurStateEnum.INITIAL,
+};
+export function utilisateurReducer(
+  state: UtilisteurState = initState,
+  action: Action
+): UtilisteurState {
+  switch (action.type) {
+    case UtilisateurActionsTypes.GET_ALL_PROPRIETAIRES:
+      return { ...state, dataState: UtilisteurStateEnum.LOADING };
+    case UtilisateurActionsTypes.GET_ALL_PROPRIETAIRES_SUCCES:
+      return {
+        ...state,
+        dataState: UtilisteurStateEnum.LOADED,
+        utlisisateurs: (<UtilisateurActions>action).payload,
+      };
+    case UtilisateurActionsTypes.GET_ALL_PROPRIETAIRES_ERROR:
+      return {
+        ...state,
+        dataState: UtilisteurStateEnum.ERROR,
+        errorMessage: (<UtilisateurActions>action).payload,
+      };
+    default:
+      return { ...state };
+  }
+}
