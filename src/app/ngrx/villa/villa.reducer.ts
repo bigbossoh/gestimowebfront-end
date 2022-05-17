3aaa650662d9d817a8712194f41bcbcfebc6a858
zrangonazaire <astairenazaire@gmail.com>
2022-05-17 17:23:33 +0000

    finish add type bien all$

---
+++ src/app/ngrx/villa/villa.reducer.ts
@@ -0,0 +1,53 @@
+import { Action } from '@ngrx/store';
+import { BienImmobilierDto, EtageDto, ImmeubleDto, StudioDto, VillaDto } from 'src/gs-api/src/models';
+import {
+  VillaActions,
+  VillaActionsTypes,
+} from './villa.action';
+export enum VillaStateEnum {
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+  NEW = 'New',
+  EDIT = 'Edit',
+  LOADEDBYIMMEUNLE = 'LoadedByImmeuble'
+
+}
+export interface VillaState {
+  villas: VillaDto[];
+  errorMessage: string;
+  dataState: VillaStateEnum;
+}
+const initState: VillaState = {
+  villas: [],
+  errorMessage: '',
+  dataState: VillaStateEnum.INITIAL,
+};
+export function villaReducer(
+  state: VillaState = initState,
+  action: Action
+): VillaState {
+  switch (action.type) {
+
+    //SAVE ETAGE
+    case VillaActionsTypes.SAVE_VILLA:
+      return { ...state, dataState: VillaStateEnum.LOADING };
+    case VillaActionsTypes.SAVE_VILLA_SUCCES:
+      let vill: StudioDto[] = [...state.villas];
+      vill.push((<VillaActions>action).payload)
+      return {
+        ...state,
+        dataState: VillaStateEnum.LOADED,
+        villas: vill
+      };
+    case VillaActionsTypes.SAVE_VILLA_ERROR:
+      return {
+        ...state,
+        dataState: VillaStateEnum.ERROR,
+        errorMessage: (<VillaActions>action).payload,
+      };
+    default:
+      return { ...state };
+  }
+}
