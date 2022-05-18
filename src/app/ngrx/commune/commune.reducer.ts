b41ee6af043234b34ab00850bae87697b05c6eca
zrangonazaire <astairenazaire@gmail.com>
2022-05-18 11:18:28 +0000

    Fini affecter list des biens

---
+++ src/app/ngrx/commune/commune.reducer.ts
@@ -0,0 +1,51 @@
+import { Action } from '@ngrx/store';
+import { BienImmobilierDto, CommuneDto, EtageDto, ImmeubleDto } from 'src/gs-api/src/models';
+import {
+  CommunesActions,
+  CommunesActionsTypes,
+} from './commune.actions';
+export enum CommunesStateEnum {
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+  NEW = 'New',
+  EDIT = 'Edit',
+  LOADEDBYIMMEUNLE = 'LoadedByImmeuble'
+
+}
+export interface CommunesState {
+  communes: CommuneDto[];
+  errorMessage: string;
+  dataState: CommunesStateEnum;
+}
+const initState: CommunesState = {
+  communes: [],
+  errorMessage: '',
+  dataState: CommunesStateEnum.INITIAL,
+};
+export function communeReducer(
+  state: CommunesState = initState,
+  action: Action
+): CommunesState {
+  switch (action.type) {
+    case CommunesActionsTypes.GET_ALL_COMMUNE_BY_VILLE:
+      return { ...state, dataState: CommunesStateEnum.LOADING };
+    case CommunesActionsTypes.GET_ALL_COMMUNE_BY_VILLE_SUCCES:
+      return {
+        ...state,
+        dataState: CommunesStateEnum.LOADED,
+        communes: (<CommunesActions>action).payload,
+      };
+    case CommunesActionsTypes.GET_ALL_COMMUNE_BY_VILLE_ERROR:
+      return {
+        ...state,
+        dataState: CommunesStateEnum.ERROR,
+        errorMessage: (<CommunesActions>action).payload,
+      };
+
+
+    default:
+      return { ...state };
+  }
+}
