84bc1c776e8c7912774e23a420db4354b6675c8d
zrangonazaire <astairenazaire@gmail.com>
2022-05-25 11:28:55 +0000

    update dto

--- src/app/pages/baux/page-baux/page-baux.component.ts
+++ src/app/pages/baux/page-baux/page-baux.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GetAllOperationActions } from 'src/app/ngrx/baux/baux.actions';
+import { BauxState, BauxStateEnum } from 'src/app/ngrx/baux/baux.reducer';
 
 @Component({
   selector: 'app-page-baux',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-baux.component.css']
 })
 export class PageBauxComponent implements OnInit {
+  bauxState$: Observable<BauxState> | null = null;
 
-  constructor() { }
+  readonly BauxStateEnum = BauxStateEnum;
+
+  constructor(private store: Store<any>) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new GetAllOperationActions({}));
+    this.bauxState$ = this.store.pipe(map((state) => state.bauxState));
   }
-
+  onActionEmmit($event: any) {
+    this.ngOnInit();
+  }
 }
--- src/gs-api/src/models/commune-dto.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-export interface CommuneDto {
-  id?: number;
-  idAgence?: number;
-  abrvCommune?: string;
-  nomCommune?: string;
-  idVille?: number;
-}
