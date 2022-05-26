6a4b10ad3987cac64acec25573f667bdb715d177
zrangonazaire <astairenazaire@gmail.com>
2022-05-26 13:36:37 +0000

    upd

---
+++ src/app/ngrx/appelloyer/appelloyer.reducer.ts
@@ -0,0 +1,52 @@
+import { Action } from '@ngrx/store';
+import { AppelLoyer, OperationDto } from 'src/gs-api/src/models';
+import {
+  AppelLoyerActions,
+  AppelLoyerctionsTypes,
+} from './appelloyer.actions';
+export enum AppelLoyerStateEnum {
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+  NEW = 'New',
+  EDIT = 'Edit',
+  LOADEDBYIMMEUNLE = 'LoadedByImmeuble'
+
+}
+export interface AppelLoyerState {
+  appelloyers: AppelLoyer[];
+  errorMessage: string;
+  dataState: AppelLoyerStateEnum;
+}
+const initState: AppelLoyerState = {
+  appelloyers: [],
+  errorMessage: '',
+  dataState: AppelLoyerStateEnum.INITIAL,
+};
+export function appelLoyerReducer(
+  state: AppelLoyerState = initState,
+  action: Action
+): AppelLoyerState {
+  switch (action.type) {
+
+
+    // GET ALL APPARTEMENT
+    case AppelLoyerctionsTypes.GET_ALL_APPELLOYER:
+      return { ...state, dataState: AppelLoyerStateEnum.LOADING };
+    case AppelLoyerctionsTypes.GET_ALL_APPELLOYER_SUCCES:
+      return {
+        ...state,
+        dataState: AppelLoyerStateEnum.LOADED,
+        appelloyers: (<AppelLoyerActions>action).payload,
+      };
+    case AppelLoyerctionsTypes.GET_ALL_APPELLOYER_ERROR:
+      return {
+        ...state,
+        dataState: AppelLoyerStateEnum.ERROR,
+        errorMessage: (<AppelLoyerActions>action).payload,
+      };
+    default:
+      return { ...state };
+  }
+}
