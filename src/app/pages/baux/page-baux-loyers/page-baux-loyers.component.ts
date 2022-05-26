77d1f771264b09b37947e28b6c04bcf56bea9439
zrangonazaire <astairenazaire@gmail.com>
2022-05-26 13:36:37 +0000

    upd

--- src/app/pages/baux/page-baux-loyers/page-baux-loyers.component.ts
+++ src/app/pages/baux/page-baux-loyers/page-baux-loyers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GetAllOperationActions } from 'src/app/ngrx/baux/baux.actions';
+import { BauxState, BauxStateEnum } from 'src/app/ngrx/baux/baux.reducer';
 
 @Component({
   selector: 'app-page-baux-loyers',
@@ -6,10 +11,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-baux-loyers.component.css']
 })
 export class PageBauxLoyersComponent implements OnInit {
+  bauxState$: Observable<BauxState> | null = null;
 
-  constructor() { }
-
+  readonly BauxStateEnum = BauxStateEnum;
+  constructor(private store: Store<any>) { }
   ngOnInit(): void {
+    this.store.dispatch(new GetAllOperationActions({}));
+    this.bauxState$ = this.store.pipe(map((state) => state.bauxState));
   }
-
 }
