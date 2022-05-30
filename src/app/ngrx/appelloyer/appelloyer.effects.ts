82d00909344236f7cac5072a8173dfcd6403ef9c
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2022-05-30 12:37:32 +0000

    Update ide to windows

--- src/app/ngrx/appelloyer/appelloyer.effects.ts
+++ src/app/ngrx/appelloyer/appelloyer.effects.ts
@@ -4,24 +4,28 @@ import { Action } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
 import { ApiService } from 'src/gs-api/src/services';
-import { GetAllAppartementActionsError, GetAllAppartementActionsSuccess } from '../appartement/appartement.actions';
-import { AppelLoyerctionsTypes, GetAllAppelLoyerActionsError, GetAllAppelLoyerActionsSuccess } from './appelloyer.actions';
+import {
+  GetAllAppartementActionsError,
+  GetAllAppartementActionsSuccess,
+} from '../appartement/appartement.actions';
+import {
+  AppelLoyerctionsTypes,
+  GetAllAppelLoyerActionsError,
+  GetAllAppelLoyerActionsSuccess,
+  AppelLoyerActions,
+} from './appelloyer.actions';
 
 @Injectable()
 export class AppelLoyerEffects {
-  constructor(private apiService: ApiService, private effectActions: Actions) { }
-
+  constructor(private apiService: ApiService, private effectActions: Actions) {}
 
   //LISTE DES APPARTEMENT
   getAllBauxEffect: Observable<Action> = createEffect(() =>
     this.effectActions.pipe(
       ofType(AppelLoyerctionsTypes.GET_ALL_APPELLOYER),
-      mergeMap((action) => {
-        return this.apiService.listDesLoyersParBail(action).pipe(
-          map(
-            (appelloyers) =>
-              new GetAllAppelLoyerActionsSuccess(appelloyers)
-          ),
+      mergeMap((action: AppelLoyerActions) => {
+        return this.apiService.listDesLoyersParBail(action.payload).pipe(
+          map((appelloyers) => new GetAllAppelLoyerActionsSuccess(appelloyers)),
           catchError((err) => of(new GetAllAppelLoyerActionsError(err.message)))
         );
       })
--- src/gs-api/src/models/magasin-dto.ts
+++ src/gs-api/src/models/magasin-dto.ts
@@ -14,7 +14,7 @@ export interface MagasinDto {
   idEtage?: number;
   idSite?: number;
   idUtilisateur?: number;
-  archived?: boolean;
   occupied?: boolean;
+  archived?: boolean;
   underBuildingMagasin?: boolean;
 }
