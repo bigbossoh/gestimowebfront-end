67a10ef7dc74044e89df4ff694cce1f1304bf24c
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2022-09-29 17:30:16 +0000

    push du 29 09 2022

---
+++ src/app/chart/chart-camanber-impaye-mois/chart-camanber-impaye-mois.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-camanber-impaye-mois',
+  templateUrl: './chart-camanber-impaye-mois.component.html',
+  styleUrls: ['./chart-camanber-impaye-mois.component.css']
+})
+export class ChartCamanberImpayeMoisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
