43be40f8c061b17b5f650fbdcd000a348231191e
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2023-02-13 15:32:04 +0000

    eee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
 
-   serverUrl:'http://209.250.224.13:8282/actuator'
-  // serverUrl:
-  //   'http://localhost:8282/actuator',
+ //  serverUrl:'http://209.250.224.13:8282/actuator'
+  serverUrl:
+    'http://localhost:8282/actuator',
 };
 
 /*
