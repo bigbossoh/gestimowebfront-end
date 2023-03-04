604e1d57851690e76b32fab4d94aafe1cd147089
zrangonazaire <astairenazaire@gmail.com>
2023-03-04 10:34:21 +0000

    push du samedi

--- src/gs-api/src/api-configuration.ts
+++ src/gs-api/src/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://209.250.224.13:8282/';
+  rootUrl: string = 'http://localhost:8282/';
 }
 
 export interface ApiConfigurationInterface {
