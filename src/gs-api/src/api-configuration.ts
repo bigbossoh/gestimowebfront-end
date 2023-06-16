cbca1f236b6b03f90059b13d0f2d64ed819a5938
zrangonazaire <astairenazaire@gmail.com>
2023-06-16 17:29:15 +0000

    recu bis

--- src/gs-api/src/api-configuration.ts
+++ src/gs-api/src/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8282/';
+  rootUrl: string = 'http://185.124.203.69:8282/';
 }
 
 export interface ApiConfigurationInterface {
