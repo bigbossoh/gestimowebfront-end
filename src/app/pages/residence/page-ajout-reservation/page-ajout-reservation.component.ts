c4ab5843e0501af8baff9107ffd187e0fca0d985
Michel BOSSOH <michel.bossoh@outlook.fr>
2023-07-04 23:29:50 +0000

    design les page our la reservation des residence

---
+++ src/app/pages/residence/page-ajout-reservation/page-ajout-reservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-ajout-reservation',
+  templateUrl: './page-ajout-reservation.component.html',
+  styleUrls: ['./page-ajout-reservation.component.css']
+})
+export class PageAjoutReservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
